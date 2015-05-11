8d75fd71133fdef08d201c7268d82421fd40eef5
Mu-An Chiou <me@muanchiou.com>
2015-05-11 22:02:08 +0800

    How to JavaScript

--- assets/stuff.js
+++ assets/stuff.js
@@ -3,11 +3,11 @@
 $(document).pjax("a", ".wrapper", {fragment: ".wrapper"})
 
 $(document).on("click", "a[id]", function (e) {
-  _gaq.push ["_trackEvent", "Clicks", "clicked on " + e.target.id]
+  _gaq.push(["_trackEvent", "Clicks", "clicked on " + e.target.id])
 })
 
 $(document).on("click", "a:not([id])", function (e) {
-  _gaq.push ["_trackEvent", "Clicks", "clicked on " + $(this).text()]
+  _gaq.push(["_trackEvent", "Clicks", "clicked on " + $(this).text()])
 })
 
 $(document).on("ready pjax:end", function () {
