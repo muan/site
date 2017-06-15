68debc049ee83f365ecc7d77d65aa1175e0bcc94
Mu-An Chiou <me@muanchiou.com>
2017-06-16 07:45:43 +0800

    Link to home and.. others

--- assets/stuff.js
+++ assets/stuff.js
@@ -1,4 +1,4 @@
-const emoji = ["🐝", "🌵", "🐧", "🥑", "🍅", "️🏡", "👩‍💻", "🎮"]
+const emoji = ["🐝", "🌵", "🐧", "🥑", "🍅", "🏡", "👩‍💻", "🎮"]
 const randomEmoji = document.querySelector(".js-random-emoji")
 
 if (randomEmoji) {
