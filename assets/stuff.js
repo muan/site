1ab99aa840cf3a0cbb7744bb26ceb5f116d249ae
Mu-An ✌️ Chiou <muan@github.com>
2018-06-22 22:49:05 -0700

    Update emoji

--- assets/stuff.js
+++ assets/stuff.js
@@ -1,4 +1,4 @@
-const emoji = ['🐝', '🌵', '🐧', '🥑', '🍅', '🏡', '👩‍💻', '🎮']
+const emoji = ['🌚', '🌵', '🐧', '🙈', '🍅', '🍳', '🙎🏻', '🎮']
 const randomEmoji = document.querySelector('.js-random-emoji')
 
 if (randomEmoji) {
