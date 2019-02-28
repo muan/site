c7591bd0e3fd60fc473e06cc66e469cc6883033d
Mu-An Chiou <me@muanchiou.com>
2019-02-28 00:22:14 -0500

    Default dark mode for dark mode peeps

--- assets/stuff.js
+++ assets/stuff.js
@@ -5,15 +5,16 @@ if (randomEmoji) {
   randomEmoji.textContent = emoji[Math.floor(Math.random() * emoji.length)]
 }
 
+const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
 const supportsLocalStorage = 'localStorage' in window
 const darkModeCheckbox = document.querySelector('#dark-mode')
 if (supportsLocalStorage) {
   darkModeCheckbox.addEventListener('change', rememberMode)
-  const darkModeOn = localStorage.getItem('darkMode')
-  if (darkModeOn) darkModeCheckbox.checked = true
+  const darkMode = localStorage.getItem('darkMode')
+  if (darkMode === '1' || (osDarkMode && darkMode === null)) darkModeCheckbox.checked = true
 
   function rememberMode () {
-    darkModeCheckbox.checked ? localStorage.setItem('darkMode', true) : localStorage.removeItem('darkMode')
+    darkModeCheckbox.checked ? localStorage.setItem('darkMode', '1') : localStorage.setItem('darkMode', '0')
   }
 }
 
