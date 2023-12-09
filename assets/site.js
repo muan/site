d28181c8c5d5c787aa5989eb84dfc6f3aefaf44b
Dhruv Suthar <intelligentjimmy@gmail.com>
2023-12-09 10:02:58 +0530

    fix: use hourCycle for Intl.DateTimeFormats

--- assets/site.js
+++ assets/site.js
@@ -5,14 +5,12 @@ function settime() {
   const options = {
     timeZone: "Asia/Taipei",
     timeStyle: "short",
-    hour12: false
+    hour12: false,
+    hourCycle: "h23"
   }
 
   // https://gist.github.com/muan/e7414b6241f088090acd916ed965540e
   let time = new Intl.DateTimeFormat(navigator.language || "zh-TW", options).format(new Date())
-
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=1262801
-  if (time.match(/^24:/)) time = time.replace('24:', '00:')
 
   // Setting interpolated string instead of just the time because
   // if there's no JS there should be no mentions of current time
