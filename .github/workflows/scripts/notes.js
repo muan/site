914c90e11d27efa27a9347a6c30064782e6a1e28
Mu-An Chiou <github@muan.dev>
2024-11-02 10:33:03 +0800

    Add tags and lang

--- .github/workflows/scripts/notes.js
+++ .github/workflows/scripts/notes.js
@@ -1,7 +1,9 @@
 import fs from 'fs'
 
 const datetime = process.argv[2]
-const content = process.argv[3]
+const tags = process.argv[3]
+const lang = process.argv[4]
+const content = process.argv[5]
 const alphabets = [...'umoacenrvwz']
 const idx = fs.readdirSync('_notes').filter(p => p.startsWith(datetime.split(' ')[0])).length
 const a = alphabets[idx]
@@ -11,6 +13,7 @@ title: Note
 layout: default
 open_heart: true
 date: ${datetime}
+tags: ${tags.split(',').map(tag => `\n  - ${tag}`)}${lang ? `\nlang: ${lang}` : ''}
 ---
 
 ${content}
