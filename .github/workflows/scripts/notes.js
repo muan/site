7ea3d2df8617629b45ca298736da5145dfa64ad3
Mu-An Chiou <github@muan.dev>
2024-11-23 20:34:48 +0800

    Because

--- .github/workflows/scripts/notes.js
+++ .github/workflows/scripts/notes.js
@@ -5,7 +5,7 @@ const tags = process.argv[3]
 const lang = process.argv[4]
 const location = process.argv[5] || null
 const content = process.argv[6]
-const alphabets = [...'umoacenrvwz']
+const alphabets = [...'acefmnortuvwyz']
 const idx = fs.readdirSync('_notes').filter(p => p.startsWith(datetime.split(' ')[0])).length
 const a = alphabets[idx]
 const filePath = `_notes/${datetime.split(' ')[0]}-${a}${a}.md`
