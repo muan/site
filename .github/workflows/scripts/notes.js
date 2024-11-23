8472a6a188019d6cabbe22c435f6186dce03c17c
Mu-An Chiou <me@muanchiou.com>
2024-11-23 12:18:08 +0800

    Add location based on TZ

--- .github/workflows/scripts/notes.js
+++ .github/workflows/scripts/notes.js
@@ -3,7 +3,8 @@ import fs from 'fs'
 const datetime = process.argv[2]
 const tags = process.argv[3]
 const lang = process.argv[4]
-const content = process.argv[5]
+const location = process.argv[5] || null
+const content = process.argv[6]
 const alphabets = [...'umoacenrvwz']
 const idx = fs.readdirSync('_notes').filter(p => p.startsWith(datetime.split(' ')[0])).length
 const a = alphabets[idx]
@@ -13,6 +14,7 @@ title: Note
 layout: default
 open_heart: true
 date: ${datetime}
+location: ${location}
 tags: ${tags.split(',').map(tag => `\n  - ${tag}`).join('')}${lang ? `\nlang: ${lang}` : ''}
 ---
 
